d0fe88e832fb8473b738b6fb17caaa132a41f802
Violeta-star-art <79420573+Violeta-star-art@users.noreply.github.com>
2021-02-28 21:41:34 +0100

    Add files via upload

---
+++ Project3Home/src/com/company/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.company;
+
+import com.sun.xml.internal.bind.v2.runtime.Name;
+
+import javax.xml.namespace.QName;
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        sayHello( " Violeta");
+        sayGoodbye( " Team");
+
+    }
+
+    public static void sayHello(String name){
+        System.out.println("Hello " +name);
+    }
+    public static void sayGoodbye(String name){
+        System.out.println("Good bye" +name);
+    }
+}
---
+++ Project3Home/src/validations/RegistrationValidations.java
@@ -0,0 +1,13 @@
+package validations;
+
+public class RegistrationValidations {
+
+
+public static void main(String[] args) {
+    System.out.println("Copy cat");
+    System.out.println("Registration in base");
+    System.out.println("First come Last Serve");
+}
+}
+
+
