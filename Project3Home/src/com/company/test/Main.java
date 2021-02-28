0dc767956f0e427fe12f8f9ffc348c0c7d997ac9
Violeta-star-art <violetateova@gmail.com>
2021-03-01 00:14:42 +0100

    Change in price

--- Project3Home/src/com/company/test/Main.java
+++ Project3Home/src/com/company/test/Main.java
@@ -3,7 +3,7 @@ package com.company.test;
 public class Main {
     public static void main(String[] args) {
 
-        int price = 150;
+        int price = 500;
         int discountPercentage = 5;
         float productPriceAfterDiscount = price - price * (discountPercentage / 100.0f);
         System.out.println(productPriceAfterDiscount);
